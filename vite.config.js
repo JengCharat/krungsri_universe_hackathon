d0ca0390eb3d03b6d538940619f95ee5ce3bc8f1
JengCharat <zazajeng401@gmail.com>
2025-08-15 22:04:03 +0700

    can show cross the mobile

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css',
+            input: [
+                'resources/css/app.css',
                 'resources/js/app.jsx',
             ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',     // ให้ฟังทุก IP
+        port: 5173,          // หรือพอร์ตที่ต้องการ
+        cors: true,          // อนุญาตให้ cross-origin
+        https: false,
+        hmr: {
+            host: '192.168.10.116', // ใส่ IP เครื่องคุณ เช่น 192.168.1.10
+        }
+    },
 });
